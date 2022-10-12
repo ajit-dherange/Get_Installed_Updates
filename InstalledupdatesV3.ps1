3eecad0f0a288408bddee9e009f403dcead5265f
Ajit Dherange <45383984+ajit-dherange@users.noreply.github.com>
2022-10-13 01:45:39 +0800

    Add files via upload

---
+++ InstalledupdatesV3.ps1
@@ -0,0 +1,13 @@
+﻿ # Get Hotfixes Installed on the Servers
+  
+ $Servers=Get-Content D:\PS-Scripts\Installed-Updates\Servers.txt
+
+ foreach ($Hostname in $Servers) 
+ {
+  $Date=(Get-Date).ToString("yyyyMMdd")
+  $Out="D:\PS-Scripts\Installed-Updates\"+$Hostname+"-installedupdates-"+$Date+".csv"
+  get-hotfix | select PSComputerName,Description,HotFixID,InstalledOn | export-csv $Out
+  }
+
+  # get-hotfix | select PSComputerName,Description,HotFixID | export-csv c:\$env:COMPUTERNAME-installedupdates.csv
+
